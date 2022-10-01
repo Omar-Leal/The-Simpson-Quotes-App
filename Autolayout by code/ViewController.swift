fbff3ea73b212d3ea33a96605acff344db9745ca
Omar Leal <omarenriquelealr@gmail.com>
2022-10-01 13:43:04 -0400

    Initial Commit

---
+++ Autolayout by code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autolayout by code
+//
+//  Created by Omar Leal on 10/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
